daa781a202de2d2dd054413d9fac97284653f65b
Ruby <rubyseresin@gmail.com>
2023-11-28 11:48:22 +0000

    updated main program to use recipe classes

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ connection.connect()
 # Seed with some seed data
 connection.seed("seeds/music_library.sql")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
+recipe_repository = RecipeRepository(connection)
+recipes = recipe_repository.all()
+for recipe in recipes:
+    print(recipe)
